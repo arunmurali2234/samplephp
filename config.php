5ced3c294ae0cbb1220df3b9f3cfd42aa5dc652e
root <root@ip-172-31-23-155.eu-north-1.compute.internal>
2024-05-05 04:27:32 +0000

    all the files updated

--- config.php
+++ config.php
@@ -1,13 +1,20 @@
 <?php
 define("DB_SERVER", "localhost");
 define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "Arun@1234");
 define("DB_NAME", "registered");
+$port = 3306; //default port 3306
 
-# Connection
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+// Connection
+// Connection
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, $port);
 
-# Check connection
-if (!$link) {
-  die("Connection failed: " . mysqli_connect_error());
+// Check connection
+if ($link === false) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Set charset
+if (!mysqli_set_charset($link, "utf8")) {
+    die("Error loading character set utf8: " . mysqli_error($link));
 }
